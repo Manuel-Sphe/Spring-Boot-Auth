d57d24ad1aec41937981ccc5f189c207691814d9
Sphesihle Madonsela <sphesihlemanuel99@gmail.com>
2023-02-13 16:52:13 +0200

    Auth working

--- src/main/java/dev/sphe/jwtdemo/JwtDemoApplication.java
+++ src/main/java/dev/sphe/jwtdemo/JwtDemoApplication.java
@@ -1,8 +1,11 @@
 package dev.sphe.jwtdemo;
 
+import dev.sphe.jwtdemo.confing.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class JwtDemoApplication {
 
